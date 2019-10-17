14502fb3e00e4b88380bcc3d00ee7de2fc4e208e
王銘熙 <karta571110@gmail.com>
2019-10-17 19:34:25 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            int guess = 55;
+
+            while (true)
+            {
+                string input_str = Console.ReadLine();
+                int guess = new Random().Next(100); //0-100
+                input = int.Parse(input_str);
+
+
+                if (inprt==guess)
+                {
+                    break;
+                }
+                Console.WriteLine("gg");
+            }
+            
+            
+            Console.ReadKey();
+
+        }
+    }
+}
